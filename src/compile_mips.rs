4a2de5eab3611447c9fcec7fb10032f4b960aec2
Daan de Graaf <daan.de.graaf@prodrive-technologies.com>
2018-11-02 14:17:56 +0100

    Compile hello world

--- src/compile_mips.rs
+++ src/compile_mips.rs
@@ -1,31 +1,60 @@
-use names::{Generator, Name};
-use parse::{Document, Expr, Ident, Lit, Op};
+use names::Generator;
+use parse::{Document, Expr, Lit, Op, Stmt};
 
 use std::collections::HashMap;
-
-pub type MipsDoc = Vec<MipsInst>;
+use std::fmt::{Display, Error as FormatError, Formatter};
 
 #[derive(Debug)]
 pub enum MipsInst {
     La(Register, Label),
     Add(Register, Register, Register),
     Sub(Register, Register, Register),
-    Slt(Register, Register, Register),
+    Seq(Register, Register, Register),
     Move(Register, Register),
     Jal(Label),
+    Li(Register, i32),
+    Jr(Register),
+    SysCall,
 }
 use self::MipsInst::*;
 
+impl Display for MipsInst {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FormatError> {
+        match self {
+            La(r, l) => write!(f, "la   {}, {}", r, l),
+            Add(a, b, c) => write!(f, "add  {}, {}, {}", a, b, c),
+            Sub(a, b, c) => write!(f, "sub  {}, {}, {}", a, b, c),
+            Seq(a, b, c) => write!(f, "seq  {}, {}, {}", a, b, c),
+            Move(a, b) => write!(f, "move {}, {}", a, b),
+            Jal(l) => write!(f, "jal  {}", l),
+            Li(r, i) => write!(f, "li   {}, {}", r, i),
+            Jr(r) => write!(f, "jr   {}", r),
+            SysCall => write!(f, "syscall"),
+        }
+    }
+}
+
 #[derive(Debug)]
-enum DataValue {
+pub enum DataValue {
     Asciiz(String),
-    Byte(Vec<u8>),
-    Word(Vec<i32>),
-    Space(usize),
+}
+
+impl Display for DataValue {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FormatError> {
+        match self {
+            DataValue::Asciiz(s) => write!(f, ".asciiz \"{}\"", s),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Label(String);
+
+impl Display for Label {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FormatError> {
+        write!(f, "{}", self.0)
+    }
+}
 
 #[derive(Default)]
 struct Compiler<'a> {
@@ -53,27 +82,22 @@ impl<'a> Compiler<'a> {
     }
 
     fn label(&mut self) -> Label {
-        Label(self.name_gen.next().unwrap())
+        Label(self.name_gen.next().unwrap().replace("-", "_"))
     }
 
     fn put_lit(&mut self, label: Label, lit: &Lit) -> (Register, Vec<MipsInst>) {
+        let reg = self.temp_reg();
         match lit {
-            Lit::Int(n) => {
-                self.data.insert(label.clone(), DataValue::Word(vec![*n]));
-            }
-            Lit::Bool(b) => {
-                self.data
-                    .insert(label.clone(), DataValue::Byte(vec![*b as u8]));
-            }
+            Lit::Int(n) => (reg, vec![Li(reg, *n)]),
+            Lit::Bool(b) => (reg, vec![Li(reg, *b as i32)]),
             Lit::Float(_) => unimplemented!(),
             Lit::Str(s) => {
                 self.data
                     .insert(label.clone(), DataValue::Asciiz(s.to_string()));
+                (reg, vec![La(reg, label)])
             }
-            Lit::List(l) => unimplemented!(),
+            Lit::List(_l) => unimplemented!(),
         }
-        let reg = self.temp_reg();
-        (reg, vec![La(reg, label)])
     }
 
     pub fn expr(&mut self, e: &Expr) -> (Register, Vec<MipsInst>) {
@@ -82,7 +106,7 @@ impl<'a> Compiler<'a> {
                 let label = self.label();
                 self.put_lit(label, l)
             }
-            Expr::Ident(i) => unimplemented!(),
+            Expr::Ident(_i) => unimplemented!(),
             Expr::BinOp(a, op, b) => {
                 let mut inst = Vec::new();
                 let (reg_a, inst_a) = self.expr(&a);
@@ -94,7 +118,7 @@ impl<'a> Compiler<'a> {
                 match op {
                     Op::Add => inst.push(Add(reg, reg_a, reg_b)),
                     Op::Min => inst.push(Sub(reg, reg_a, reg_b)),
-                    Op::Eq => inst.push(Slt(reg, reg_a, reg_b)),
+                    Op::Eq => inst.push(Seq(reg, reg_a, reg_b)),
                 };
                 (reg, inst)
             }
@@ -112,16 +136,83 @@ impl<'a> Compiler<'a> {
             }
         }
     }
+
+    pub fn stmt(&mut self, stmt: &Stmt) -> Vec<MipsInst> {
+        match stmt {
+            Stmt::Expr(e) => self.expr(e).1,
+            Stmt::Fun(fname, _params, body) => {
+                let mut body_inst = Vec::new();
+                for stmt in body {
+                    body_inst.extend(self.stmt(stmt));
+                }
+                body_inst.push(Jr(Ra));
+                let fname = if fname.0 == "main" {
+                    "program_main".to_string()
+                } else {
+                    fname.0.clone()
+                };
+                self.text.insert(Label(fname), body_inst);
+                // Put into the text section instead
+                vec![]
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn program(self) -> MipsProgram {
+        MipsProgram(self.data, self.text)
+    }
+}
+
+#[derive(Debug)]
+pub struct MipsProgram(HashMap<Label, DataValue>, HashMap<Label, Vec<MipsInst>>);
+
+impl Display for MipsProgram {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FormatError> {
+        writeln!(f, ".data")?;
+        for (label, value) in self.0.iter() {
+            writeln!(f, "    {}:", label)?;
+            writeln!(f, "        {}", value)?;
+        }
+
+        writeln!(f, ".text")?;
+
+        writeln!(
+            f,
+            r#"
+# BEGIN Standard library
+    main:
+        jal  program_main
+        li   $v0, 10
+        syscall
+    print:
+        li   $v0, 4
+        syscall
+        jr   $ra
+# END Standard library
+                 "#
+        )?;
+
+        for (label, body) in self.1.iter() {
+            writeln!(f, "    {}:", label)?;
+            for inst in body {
+                writeln!(f, "        {}", inst)?;
+            }
+            writeln!(f, "")?;
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
 pub struct CompileError;
 
-pub fn compile(document: Document) -> Result<MipsDoc, CompileError> {
-    let compiled = Vec::new();
-
-    unimplemented!();
-    Ok(compiled)
+pub fn compile(document: Document) -> Result<MipsProgram, CompileError> {
+    let mut compiler = Compiler::new();
+    for statement in document {
+        compiler.stmt(&statement);
+    }
+    Ok(compiler.program())
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -160,6 +251,46 @@ pub enum Register {
     Ra,
 }
 use self::Register::*;
+
+impl Display for Register {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FormatError> {
+        let s = match self {
+            Zero => "$zero",
+            At => "$at",
+            V0 => "$v0",
+            V1 => "$v1",
+            A0 => "$a0",
+            A1 => "$a1",
+            A2 => "$a2",
+            A3 => "$a3",
+            T0 => "$t0",
+            T1 => "$t1",
+            T2 => "$t2",
+            T3 => "$t3",
+            T4 => "$t4",
+            T5 => "$t5",
+            T6 => "$t6",
+            T7 => "$t7",
+            S0 => "$s0",
+            S1 => "$s1",
+            S2 => "$s2",
+            S3 => "$s3",
+            S4 => "$s4",
+            S5 => "$s5",
+            S6 => "$s6",
+            S7 => "$s7",
+            T8 => "$t8",
+            T9 => "$t9",
+            K0 => "$k0",
+            K1 => "$k1",
+            Gp => "$gp",
+            Sp => "$sp",
+            Fp => "$fp",
+            Ra => "$ra",
+        };
+        write!(f, "{}", s)
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use toy_compiler::compile_mips;
 use toy_compiler::parse;
 
 fn main() {
-    println!("Hello, world!");
-
     let program = r#"
 fun main():
+    say_hello()
+
+fun say_hello():
     print("Hello, world!")
 "#;
     let parsed = parse::parse(program).expect("failed to parse");
     let compiled = compile_mips::compile(parsed).expect("failed to compile for mips");
+    println!("{}", compiled);
 }
